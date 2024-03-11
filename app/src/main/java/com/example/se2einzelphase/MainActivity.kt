bbb947fac94f31c02a39302f8d73ab231eab048e
Michael Stücklschweiger <michael.stuecklschweiger@gmail.com>
2024-03-11 08:38:51 +0100

    Added UI to app

--- app/src/main/java/com/example/se2einzelphase/MainActivity.kt
+++ app/src/main/java/com/example/se2einzelphase/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.se2einzelphase
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.se2einzelphase.ui.theme.SE2EinzelphaseTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +21,50 @@ class MainActivity : ComponentActivity() {
             SE2EinzelphaseTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    AppContent()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SE2EinzelphaseTheme {
-        Greeting("Android")
+fun AppContent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TopAppBar(
+            title = { Text("NetworkTest") }
+        )
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+            Text("Gib deine Matrikelnummer ein:")
+            var input by remember { mutableStateOf("") }
+            OutlinedTextField(
+                value = input,
+                onValueChange = { newInput ->
+                    if (newInput.all { it.isDigit() }) input = newInput
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                modifier = Modifier.fillMaxWidth()
+                    .padding(0.dp, 16.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = { /* Handle click */ }) {
+                Text("Abschicken")
+            }
+        }
     }
 }
